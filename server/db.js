3ed97322aa3e26b8f1d722b1fe679b4b3219f7a3
Zuby Javed <zuby@Zubys-MacBook-Pro.local>
2022-11-06 00:18:42 -0700

    try removing port

--- server/db.js
+++ server/db.js
@@ -6,8 +6,8 @@ const pool = new Pool({
     user: process.env.RDS_USER,
     password: process.env.RDS_PASSWORD, 
     host: process.env.RDS_HOSTNAME,
-    port: parseInt(process.env.RDS_PORT),
-    // database: process.env.DATABASE,
+    // port: parseInt(process.env.RDS_PORT),
+    database: process.env.RDS_DATABASE,
 });
 
 module.exports = pool;
