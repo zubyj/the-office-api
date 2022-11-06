0f7c44d7571aca190a3077be9abeef8a88053f33
Zuby Javed <zuby@Zubys-MacBook-Pro.local>
2022-11-06 00:33:41 -0700

    rm port and db from db.js

--- server/db.js
+++ server/db.js
@@ -6,8 +6,8 @@ const pool = new Pool({
     user: process.env.RDS_USER,
     password: process.env.RDS_PASSWORD, 
     host: process.env.RDS_HOSTNAME,
-    port: parseInt(process.env.RDS_PORT),
-    database: process.env.RDS_DATABASE,
+    // port: parseInt(process.env.RDS_PORT),
+    // database: process.env.DATABASE,
 });
 
 module.exports = pool;
