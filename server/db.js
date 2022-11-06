2fe7f684f8de406b8309dc5cfbeeeafd8a5da3e0
Zuby Javed <zuby@Zubys-MacBook-Pro.local>
2022-11-06 00:07:12 -0700

    fix wrong hostname env variable

--- server/db.js
+++ server/db.js
@@ -5,7 +5,7 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
     user: process.env.RDS_USER,
     password: process.env.RDS_PASSWORD, 
-    host: process.env.RDS_HOST,
+    host: process.env.RDS_HOSTNAME,
     port: parseInt(process.env.RDS_PORT),
     // database: process.env.DATABASE,
 });
