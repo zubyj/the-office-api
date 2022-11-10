d5c65d8bfd502c4af78fd03edc287fc51c1c62af
Zuby Javed <zuby@Zubys-MacBook-Pro.local>
2022-11-09 16:05:56 -0800

    remove console log port

--- server/index.js
+++ server/index.js
@@ -8,7 +8,6 @@ require('dotenv').config()
 const cors = require("cors");
 const helmet = require("helmet");
 
-console.log('port is ' + PORT);
 const PORT = process.env.RDS_PORT;
 const pool = require('./db');
 
