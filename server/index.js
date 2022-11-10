2dfb7cada0eec7b9c2b88e84bf275e8155d408a1
Zuby Javed <zuby@Zubys-MacBook-Pro.local>
2022-11-09 22:52:52 -0800

    fix pathname for homepage

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 
 const express = require('express');
-const path = require('path');
 const app = express()
 
 require('dotenv').config()
@@ -51,7 +50,7 @@ Under set cooke security options, set httponly to true, and domain
 
 // sendFile will go here
 app.get('/', function(req, res) {
-    res.sendFile(path.join(__dirname, 'the-office-script-api-tutorial/dist/index.html'));
+    res.sendFile('the-office-script-api-tutorial/dist/index.html');
   });
 
 // Gets a random line
