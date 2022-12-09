e4944c3f4c55fad7e5813ce7cd8f4f1007825448
Zuby Javed <zubyjaved@gmail.com>
2022-12-08 23:22:50 -0800

    add vue-gtag for analytics

--- documentation/src/main.js
+++ documentation/src/main.js
@@ -1,6 +1,9 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueGtag from 'vue-gtag';
 
 import './assets/main.css'
 
-createApp(App).mount('#app')
+createApp(App).use(VueGtag, {
+    config: { id: PROCESS.env.MEASUREMENT_ID }
+}).mount('#app')
