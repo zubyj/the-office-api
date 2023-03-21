8595c2121454311ea76bb07a51b87ccea990d115
Zuby Javed <zubyjaved@gmail.com>
2023-03-21 01:21:58 -0700

    extract all random api requests from main index.js

--- server/routes/random.js
+++ server/routes/random.js
@@ -16,4 +16,92 @@ router.get('/random', async (req, res) => {
     }
 });
 
+// Gets a random quote from a random character given season and episode
+router.get("/seasons/:season/episodes/:episode/random", async (req, res) => {
+    console.log('Get random quote from random character given season and episode');
+    try {
+        const { season, episode } = req.params;
+        const seasonNum = parseInt(season);
+        const episodeNum = parseInt(episode);
+        const quote = await pool.query(
+            "SELECT character, line FROM lines WHERE season = $1 AND episode = $2 OFFSET floor(random() * (SELECT COUNT(*) FROM lines WHERE season = $1 AND episode = $2)) ",
+            [seasonNum, episodeNum]
+        );
+        res.json(quote.rows[0]);
+    }
+    catch (err) {
+        console.error(err);
+    }
+})
+
+// Gets a random quote from given character
+router.get("/characters/:character/random", async (req, res) => {
+    console.log('Get random quote from given character');
+    try {
+        const { character } = req.params;
+        const characterName = character.charAt(0).toUpperCase() + character.slice(1);
+        const quote = await pool.query(
+            "SELECT season, episode, line FROM lines WHERE character = $1 OFFSET floor(random() * (SELECT COUNT(*) FROM lines WHERE character = $1))",
+            [characterName]
+        );
+        res.json(quote.rows[0]);
+    }
+    catch (err) {
+        console.error(err);
+    }
+})
+
+// Gets a random quote from given season
+router.get("/seasons/:season/random", async (req, res) => {
+    try {
+        const { season } = req.params;
+        const seasonNum = parseInt(season);
+        const quote = await pool.query(
+            "SELECT season, episode, character, line FROM lines WHERE season = $1 OFFSET floor(random() * (SELECT COUNT(*) FROM lines WHERE season = $1)) ",
+            [seasonNum]
+        );
+        res.json(quote.rows[0]);
+    }
+    catch (err) {
+        console.error(err);
+    }
+})
+
+// Gets a random quote from given season and character
+router.get("/seasons/:season/characters/:character/random", async (req, res) => {
+    console.log('Get random quote from given season and character');
+    try {
+        const { season, character } = req.params;
+        const seasonNum = parseInt(season);
+        const characterName = character.charAt(0).toUpperCase() + character.slice(1);
+        const quote = await pool.query(
+            "SELECT episode, line FROM lines WHERE season = $1 AND character = $2 OFFSET floor(random() * (SELECT COUNT(*) FROM lines WHERE season = $1 AND character = $2)) ",
+            [seasonNum, characterName]
+        );
+        res.json(quote.rows[0]);
+    }
+    catch (err) {
+        console.error(err);
+    }
+})
+
+// Gets random quote from given season, episode, and character
+router.get("/seasons/:season/episodes/:episode/characters/:character/random", async (req, res) => {
+    console.log('Get random quote from given season, episode, and character');
+    try {
+        const { season, episode, character } = req.params;
+        const seasonNum = parseInt(season);
+        const episodeNum = parseInt(episode);
+        const characterName = character.charAt(0).toUpperCase() + character.slice(1);
+        const quote = await pool.query(
+            "SELECT line FROM lines WHERE season = $1 AND episode = $2 AND character = $3 OFFSET floor(random() * (SELECT COUNT(*) FROM lines WHERE season = $1 AND episode = $2 AND character = $3)) ",
+            [seasonNum, episodeNum, characterName]
+        );
+        res.json(quote.rows[0]);
+    }
+    catch (err) {
+        console.log(err);
+    }
+})
+
 module.exports = router;
