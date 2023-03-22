61de1a849f083acd03ca1f059e9bd0c22bdcacd8
Zuby Javed <zubyjaved@gmail.com>
2023-03-21 21:51:31 -0700

    update analytics function to use google firebase

--- server/middlewares/analytics.js
+++ server/middlewares/analytics.js
@@ -1,19 +1,31 @@
 const axios = require('axios');
 const GA_MEASUREMENT_ID = process.env.GA_MEASUREMENT_ID;
 const GA_API_SECRET = process.env.GA_API_SECRET;
+const FIREBASE_APP_ID = process.env.FIREBASE_APP_ID; // Replace with your Firebase App ID
+const APP_INSTANCE_ID = process.env.APP_INSTANCE_ID
 
 async function sendAnalyticsEvent(event) {
     try {
         const payload = {
-            app_name: 'theofficescript',
-            app_version: '1.0.0',
-            event_name: event,
+            app_instance_id: APP_INSTANCE_ID, // Replace with actual app instance ID (e.g., from Firebase SDK)
+            events: [
+                {
+                    name: event,
+                    params: {},
+                },
+            ],
         };
-        const response = await axios.post(`https://www.google-analytics.com/mp/collect?measurement_id=${GA_MEASUREMENT_ID}&api_secret=${GA_API_SECRET}`, event);
-        console.log('Sending analytics event for ' + event);
+        const response = await axios.post(
+            `https://www.google-analytics.com/mp/collect?firebase_app_id=${FIREBASE_APP_ID}&api_secret=${GA_API_SECRET}`,
+            payload,
+            {
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+            }
+        );
         console.log(response.data);
-    }
-    catch (error) {
+    } catch (error) {
         console.log(`Failed to send event to Google Analytics: ${error.message}`);
     }
 }
