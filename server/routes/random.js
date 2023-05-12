3ec2ed0309f4e7c1428376bf3f854f4fa0a4a7ce
Zuby Javed <zubyjaved@gmail.com>
2023-05-11 23:39:22 -0700

    oh my god it might be working

--- server/routes/random.js
+++ server/routes/random.js
@@ -10,7 +10,7 @@ const client = new Analytics({ writeKey: 'Q6Z0yZ9V2kIaYisKsp8sFM7hVYG3hXeW' });
 router.get('/random', async (req, res) => {
     logger.info('Get a random line');
     client.track({
-        event: 'Random Quote',
+        event: 'random_line',
         userId: 'anonymous',
     })
     try {
