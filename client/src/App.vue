b3d7d1785b060cb8da12f92f9b1f96ba505d919b
Zuby Javed <zubyjaved@gmail.com>
2023-05-21 16:33:19 -0700

    add link to chat bots

--- client/src/App.vue
+++ client/src/App.vue
@@ -13,8 +13,9 @@ import RequestForm from './components/RequestForm.vue';
         <img src="./assets/the-office-api-logo.jpg" width="400" id="title_img" />
         <h2 id="description">REST API for getting lines from NBC's " The Office"</h2>
         <div class="buttons">
+          <a href="https://zubyj.com/the-office-chat-bots" target="_blank" class="button">Chat bots</a>
+          <a href="https://reddit.com/u/the-office-bot" target="_blank" class="button">Reddit bots</a>
           <a href="https://github.com/zubyj/the-office-api" target="_blank" class="button">View the code</a>
-          <a href="https://reddit.com/u/the-office-bot" target="_blank" class="button">The Office Reddit bots</a>
         </div>
       </div>
       <RequestForm />
